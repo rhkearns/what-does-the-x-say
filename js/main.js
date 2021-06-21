38df097aaf48c604b08604cd58cf2896a7f444e7
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-06-21 09:50:07 -0500

    adjust audioElement volume

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const notFoxImg = document.querySelector("#notFox")
 notFoxImg.addEventListener("click", evt => {
   if (evt.target.id !== "notFox"){
     const audioElement = new Audio(`../audio/${evt.target.id}.mp3`)
-    audioElement.volume = .1
+    audioElement.volume = .01
     audioElement.play()
   }
 })
